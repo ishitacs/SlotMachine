5a3fce0c7266e6731634e17ad3228175889097ac
Ishita Arora <ishita.arora.cs@gmail.com>
2021-02-07 21:26:04 +0530

    SlotMachine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css'
+import SlotM from './SlotMachine'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ const App = () => {
+return (<>
+<h1 className="heading_style">Welcome to 🎰 Slot Machine Game</h1>
+<SlotM x="🍧" y="🍧" z="🍧"/>
+<SlotM x=" 😃 " y=" 😉" z=" 😃"/>
+<SlotM x="🍎" y="🍎" z="🍎"/>
+<SlotM x="🙈" y="🙊" z="🙉"/>
+<SlotM x="🎧" y="🍎" z="🎧"/>
+</>);
+ };
 
 export default App;
